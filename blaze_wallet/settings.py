b052e8ff80d0720ac2fbfc582741a48754c21722
Hesoyam91 <gerv.herrera@duocuc.cl>
2023-06-14 00:39:47 -0400

    se agrega host

--- blaze_wallet/settings.py
+++ blaze_wallet/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h_mmq)awge88*q_@f0t48pbi@usa-1$_76__th-lti&v%v0hpo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["'blaze-wallet.azurewebsites.net'"]
+ALLOWED_HOSTS = ['blaze-wallet.azurewebsites.net','127.0.0.1', 'localhost']
 
 
 # Application definition
