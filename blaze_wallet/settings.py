37664b503923bea84a9889445ca8953e80630dbd
Hesoyam91 <gerv.herrera@duocuc.cl>
2023-06-23 16:27:30 -0400

    Cambios esteticos

--- blaze_wallet/settings.py
+++ blaze_wallet/settings.py
@@ -139,4 +139,4 @@ CSRF_COOKIE_SECURE = False
 # Set SameSite attribute of CSRF cookie
 CSRF_COOKIE_SAMESITE = 'Lax'
 
-CSRF_TRUSTED_ORIGINS = ['https://blaze-wallet.azurewebsites.net','127.0.0.1', 'localhost']
+CSRF_TRUSTED_ORIGINS = ['https://blaze-wallet.azurewebsites.net']
